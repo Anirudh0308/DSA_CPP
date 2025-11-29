42724002980d4928caa9e1f1214c0adae592ad3b
Anirudh0308 <anirudhgpj360@gmail.com>
2025-11-29 13:03:59 +0530

    update dsa code

---
+++ Basic/hello.cpp
@@ -0,0 +1,11 @@
+// first program
+// This is a simple C++ program that prints "Hello, World!" to the console
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Hello, World!" << "\n";
+    cout << "\n";
+    return 0;
+}
