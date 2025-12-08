ed0fff56ae1038b725d722c03d28ffb947d9c174
Anirudh0308 <anirudhgpj360@gmail.com>
2025-12-08 11:42:01 +0530

    Longest Common Prefix

---
+++ Trie/longestCommonPrefix.cpp
@@ -0,0 +1,115 @@
+// // Longest Common Prefix Problem
+
+// // Link:- https://www.naukri.com/code360/problems/longest-common-prefix_2090383?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0
+
+// string longestCommonPrefix(vector<string> &arr, int n)
+// {
+//     // Write your code here
+//     string ans = "";
+
+//     for(int i=0; i<arr[0].length(); i++){
+//         char ch = arr[0][i];
+
+//         bool match = true;
+
+//         for(int j=1; j<n; j++){
+//             // if(arr[j].size() <= i || ch != arr[j][i]) give error
+//             if (i >= arr[j].size() || ch != arr[j][i])
+//             {
+//                 match = false;
+//                 break;
+//             }
+//         }
+//         if(match== false)
+//             break;
+//         else
+//             ans.push_back(ch);
+//     }
+//     return ans;
+// }
+
+
+// // Solve using Trie
+
+// class TrieNode{
+//     public:
+//         char data;
+//         TrieNode* children[26];
+//         int childCount;
+//         bool isTerminal;
+
+//         TrieNode(char ch){
+//             data = ch;
+//             for(int i=0; i<26; i++){
+//                 children[i] = NULL;
+//             }
+//             childCount =0;
+//             isTerminal = false;
+//         }
+// };
+
+// class Trie{
+//     public:
+//         TrieNode* root;
+
+//         trie(char ch){
+//             root = new TrieNode(ch);
+//         }
+
+//         void insertUtil(TrieNode* root, string word){
+//             if(word.length() == 0){
+//                 root->isTerminal = true;
+//                 return;
+//             }
+
+//             int index = word[0] - 'a';
+//             TrieNode* child;
+//             // present
+//             if(root -> children[index] != NULL){
+//                 child = root->children[index];
+
+//             }else{
+//                 child = new TrieNode(word[0]);
+//                 root -> childCount++;
+//                 root->children[index] = child;
+
+//             }
+//             // recurtion call
+//             insertUtil(child, word.substr(1));
+//         }
+//         void insertWord(string word){
+//             insertUtil(root, word);
+//         }
+//         void lcp(string str, string &ans){
+//             for(int i=0; i<str.length(); i++){
+//                 char ch= str[i];
+
+//                 if(root ->childCount == 1){
+//                     ans.push_back(ch);
+
+//                     int index = ch -'a';
+//                     root = root -> children[index];
+//                 }
+//                 else{
+//                     break;
+//                 }
+//                 if(root -> isTerminal)
+//                     break;
+//             }
+//         }
+// };
+
+// string longestCommonPrefix(vector<string> &arr, int n){
+//     Trie *t = new Trie('\0');
+
+//     for(int i=0; i<n; i++){
+//         t->insertWord(arr[i]);
+//     }
+//     string first = arr[0];
+
+//     sring first =arr[0];
+//     string ans ="";
+
+//     t->lcp(first, ans);
+//     return ans;
+// }
