6f90a8879826c88cd18c138153b993f9a10c39fd
Anirudh0308 <anirudhgpj360@gmail.com>
2025-12-08 12:03:16 +0530

    trie complite

--- Trie/phoneDirectory.cpp
+++ Trie/phoneDirectory.cpp
@@ -1,8 +1,8 @@
-//  Implement a phone directory
+// //  Implement a phone directory
 
-// Link:- https://www.naukri.com/code360/problems/implement-a-phone-directory_1062666?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0
+// // Link:- https://www.naukri.com/code360/problems/implement-a-phone-directory_1062666?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0
 
-vector<vector<string>> phoneDirectory(vector<string>&contactList, string &queryStr)
-{
-    //    Write your code here.
-}
+// vector<vector<string>> phoneDirectory(vector<string>&contactList, string &queryStr)
+// {
+//     //    Write your code here.
+// }
