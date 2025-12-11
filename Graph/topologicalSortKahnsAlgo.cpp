31990535bcddec188cafae8f1d67d607aa3b5f26
Anirudh0308 <anirudhgpj360@gmail.com>
2025-12-11 12:24:33 +0530

    Topological Sort ( Using Kahn's Algorithm )

---
+++ Graph/topologicalSortKahnsAlgo.cpp
@@ -0,0 +1,14 @@
+// // Topological Sort ( Using Kahn's Algorithm )
+
+// vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+//     // Write your code here
+//     unordered_map<int, list<int> > adj;
+//     for(int i=0; i<e; i++){
+//         int u = edges[i][0];
+//         int v = edges[i][1];
+
+//         adj[u].push_back(v);
+//     }
+    
+//     // write more code here
+// }
